bfce2a464a893b8b2d43b66de63c079d687e64ab
yaoxinc <yaoxinc@yonyou.com>
2017-08-19 13:54:52 +0800

    初始化affix

---
+++ src/Affix.js
@@ -0,0 +1,7 @@
+import React, { Component, PropTypes } from 'react';
+const propTypes = {};
+const defaultProps = {};
+class Affix extends Component {render(){return(<h2>Welcome use tinper-bee</h2> )}};
+Affix.propTypes = propTypes;
+Affix.defaultProps = defaultProps;
+export default Affix;
