73291df80d2d22587f9a5db06345e5c32889ee3e
yaoxinc <yaoxinc@yonyou.com>
2017-08-30 10:21:16 +0800

    修改格式

--- demo/demolist/Demo3.js
+++ demo/demolist/Demo3.js
@@ -7,20 +7,15 @@
 
 class Demo3 extends Component {
     render() {
-        return ( <
-            div className = "outer-box"
-            id = "outer-box" >
-            <
-            label > 基本的Affix， `container=tinperBeeDemo horizontal offsetTop=350 ` < /label> <
-            Affix container = { document.getElementById('tinperBeeDemo') }
-            horizontal offsetTop = { 350 } >
-            <
-            div className = 'content' >
-            <
-            span > affix < /span> <
-            /div> <
-            /Affix> <
-            /div>
+        return (
+          <div className = "outer-box"id = "outer-box" >
+            <label > 基本的Affix， `container=tinperBeeDemo horizontal offsetTop=350 ` </label> 
+            <Affix container = { document.getElementById('tinperBeeDemo') }horizontal offsetTop = { 350 } >
+              <div className = 'content' >
+                <span > affix </span>
+              </div> 
+            </Affix> 
+          </div>
         )
     }
 }
