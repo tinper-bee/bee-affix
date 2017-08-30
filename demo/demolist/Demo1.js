560138a20939b55d46abdbd768546494376b7364
yaoxinc <yaoxinc@yonyou.com>
2017-08-30 10:16:48 +0800

    调整格式

--- demo/demolist/Demo1.js
+++ demo/demolist/Demo1.js
@@ -6,16 +6,16 @@
 */
 
 class Demo1 extends Component {
-	render () {
-		return (
-			<div className="outer-box" id="outer-box">
-				<label>基本的Affix，`container=tinperBeeDemo`</label>
-				<Affix container={document.getElementById('tinperBeeDemo')}>
-					<div className='content'>
-							<span>affix</span>
-					</div>
-			  </Affix>
-			</div>
-		)
-	}
+  render () {
+    return (
+      <div className="outer-box" id="outer-box">
+        <label>基本的Affix，`container=tinperBeeDemo`</label>
+        <Affix container={document.getElementById('tinperBeeDemo')}>
+          <div className='content'>
+            <span>affix</span>
+          </div>
+        </Affix>
+      </div>
+    )
+  }
 }
--- demo/demolist/Demo3.js
+++ demo/demolist/Demo3.js
@@ -1,21 +1,26 @@
 /**
-*
-* @title horizontal Affix
-* @description 被固定时是否可以左右滑动
-*
-*/
+ *
+ * @title horizontal Affix
+ * @description 被固定时是否可以左右滑动
+ *
+ */
 
 class Demo3 extends Component {
-	render () {
-		return (
-			<div className="outer-box" id="outer-box">
-				<label>基本的Affix，`container=tinperBeeDemo horizontal offsetTop=350 `</label>
-				<Affix container={document.getElementById('tinperBeeDemo')} horizontal offsetTop={350}>
-					<div className='content'>
-							<span>affix</span>
-					</div>
-			  </Affix>
-			</div>
-		)
-	}
+    render() {
+        return ( <
+            div className = "outer-box"
+            id = "outer-box" >
+            <
+            label > 基本的Affix， `container=tinperBeeDemo horizontal offsetTop=350 ` < /label> <
+            Affix container = { document.getElementById('tinperBeeDemo') }
+            horizontal offsetTop = { 350 } >
+            <
+            div className = 'content' >
+            <
+            span > affix < /span> <
+            /div> <
+            /Affix> <
+            /div>
+        )
+    }
 }
