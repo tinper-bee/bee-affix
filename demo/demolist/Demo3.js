3d6b2a4aa2b0ce94e0292304ef9635ae47359bc1
xyoooo <yaoxinc@yonyou.com>
2018-08-20 18:02:34 +0800

    demo

--- demo/demolist/Demo3.js
+++ demo/demolist/Demo3.js
@@ -12,7 +12,8 @@ import Affix from '../../src';
 class Demo3 extends Component {
     render() {
        //请注意，这个container是为了适应tinperbee官网的布局特意设定，其他没有意外不需要传container，默认body
-        let container = document.getElementById('tinperBeeDemo');
+        let container = document.getElementsByClassName('page-container u-container example')[0]? document.getElementsByClassName('page-container u-container example')[0] : document.getElementById('tinperBeeDemo');
+
         return (
           <div className = "outer-box"id = "outer-box" >
             <label > 基本的Affix， `zIndex={2001} horizontal offsetTop=450 ` </label> 
